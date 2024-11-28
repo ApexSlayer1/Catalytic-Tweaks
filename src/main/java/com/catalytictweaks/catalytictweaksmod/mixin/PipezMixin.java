d0846637a58425dbe190e931127f7f9e2293b42e
ApexSlayer1 <christiansandal13@gmail.com>
2024-11-28 11:33:48 +0100

    added all filter options to all pipez upgrades

--- src/main/java/com/catalytictweaks/catalytictweaksmod/mixin/PipezMixin.java
+++ src/main/java/com/catalytictweaks/catalytictweaksmod/mixin/PipezMixin.java
@@ -2,69 +2,67 @@ package com.catalytictweaks.catalytictweaksmod.mixin;
 
 import de.maxhenkel.pipez.Upgrade;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.lang.reflect.Field;
 
-@Mixin(Upgrade.class)
+@Mixin(Upgrade.class)  // Target the Upgrade enum class
 public class PipezMixin {
 
-    // This injects into the static values() method where enum constants are initialized
-    @Inject(method = "values", at = @At("RETURN"))
-    private static void modifyEnumAfterInitialization(CallbackInfoReturnable<Upgrade[]> cir) {
-        // Loop through all enum constants and modify fields
+    // Inject code into the static block of the Upgrade enum
+    @Inject(method = "<clinit>", at = @At("RETURN"))
+    private static void modifyEnumAfterInitialization(CallbackInfo ci) {
+        // Modify the enum constants after they are initialized
         for (Upgrade upgrade : Upgrade.values()) {
-            // Modify the "BASIC" tier booleans as an example
-            if (upgrade.name().equals("BASIC")) {
-                System.out.println("Modifying BASIC tier booleans!");
-                try {
-                    // Modify the canChangeRedstoneMode field using reflection
-                    Field redstoneModeField = Upgrade.class.getDeclaredField("canChangeRedstoneMode");
-                    redstoneModeField.setAccessible(true); // Make the private field accessible
-                    redstoneModeField.set(upgrade, true); // Set the value to true
+            try {
+                // Access the fields using reflection
+                Field canChangeRedstoneModeField = Upgrade.class.getDeclaredField("canChangeRedstoneMode");
+                Field canChangeFilterField = Upgrade.class.getDeclaredField("canChangeFilter");
+                Field canChangeDistributionModeField = Upgrade.class.getDeclaredField("canChangeDistributionMode");
 
-                    // Modify the canChangeDistributionMode field using reflection
-                    Field distributionModeField = Upgrade.class.getDeclaredField("canChangeDistributionMode");
-                    distributionModeField.setAccessible(true); // Make the private field accessible
-                    distributionModeField.set(upgrade, true); // Set the value to true
+                // Make the fields accessible
+                canChangeRedstoneModeField.setAccessible(true);
+                canChangeFilterField.setAccessible(true);
+                canChangeDistributionModeField.setAccessible(true);
 
-                    // Modify the canChangeFilter field using reflection
-                    Field filterField = Upgrade.class.getDeclaredField("canChangeFilter");
-                    filterField.setAccessible(true); // Make the private field accessible
-                    filterField.set(upgrade, true); // Set the value to true
-
-                } catch (NoSuchFieldException | IllegalAccessException e) {
-                    // Log a simple message if an error occurs modifying the fields
-                    System.out.println("Error modifying enum fields for upgrade: " + upgrade.name());
+                // Modify the values for each upgrade
+                switch (upgrade) {
+                    case BASIC:
+                        // Set the fields for BASIC upgrade
+                        canChangeRedstoneModeField.set(upgrade, true);
+                        canChangeFilterField.set(upgrade, true);
+                        canChangeDistributionModeField.set(upgrade, true);
+                        break;
+                    case IMPROVED:
+                        // Set the fields for IMPROVED upgrade
+                        canChangeRedstoneModeField.set(upgrade, true);
+                        canChangeFilterField.set(upgrade, true);
+                        canChangeDistributionModeField.set(upgrade, true);
+                        break;
+                    case ADVANCED:
+                        // Set the fields for ADVANCED upgrade
+                        canChangeRedstoneModeField.set(upgrade, true);
+                        canChangeFilterField.set(upgrade, true);
+                        canChangeDistributionModeField.set(upgrade, true);
+                        break;
+                    case ULTIMATE:
+                        // Set the fields for ULTIMATE upgrade
+                        canChangeRedstoneModeField.set(upgrade, true);
+                        canChangeFilterField.set(upgrade, true);
+                        canChangeDistributionModeField.set(upgrade, true);
+                        break;
+                    case INFINITY:
+                        // Set the fields for INFINITY upgrade
+                        canChangeRedstoneModeField.set(upgrade, true);
+                        canChangeFilterField.set(upgrade, true);
+                        canChangeDistributionModeField.set(upgrade, true);
+                        break;
                 }
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
             }
-
-            // You can add similar blocks for other tiers (IMPROVED, ADVANCED, etc.)
-            // For example, modifying the "IMPROVED" tier can follow the same pattern
-            if (upgrade.name().equals("IMPROVED")) {
-                System.out.println("Modifying IMPROVED tier booleans!");
-                try {
-                    Field redstoneModeField = Upgrade.class.getDeclaredField("canChangeRedstoneMode");
-                    redstoneModeField.setAccessible(true);
-                    redstoneModeField.set(upgrade, true);
-
-                    Field distributionModeField = Upgrade.class.getDeclaredField("canChangeDistributionMode");
-                    distributionModeField.setAccessible(true);
-                    distributionModeField.set(upgrade, true);
-
-                    Field filterField = Upgrade.class.getDeclaredField("canChangeFilter");
-                    filterField.setAccessible(true);
-                    filterField.set(upgrade, true);
-
-                } catch (NoSuchFieldException | IllegalAccessException e) {
-                    System.out.println("Error modifying enum fields for upgrade: " + upgrade.name());
-                }
-            }
-
-            // Similarly, you can modify ADVANCED, ULTIMATE, INFINITY tiers
-            // For now, just adding the "BASIC" and "IMPROVED" tiers as examples
         }
     }
 }
